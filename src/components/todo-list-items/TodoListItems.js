d9d3e5bb991220da71bfdec0b8f6b4df7a293e54
Kpokc <pavmakarovs@gmail.com>
2021-11-16 15:38:48 +0000

    Added - TodoListItems cross out title if clicked

--- src/components/todo-list-items/TodoListItems.js
+++ src/components/todo-list-items/TodoListItems.js
@@ -4,14 +4,35 @@ import './TodoListItems.css'
 
 export default class TodoListItems extends Component{
 
+    constructor() {
+        super();
+
+        this.state = {
+            done: false
+        };
+    }
+
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        });
+    };
+
     render () {
 
         // To get access to all function props
         const { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
     
         const style = {
-            color: important ? 'tomato' : 'black'
-        }
+            color: important ? 'tomato' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
     
         return (
           <div className="row">
@@ -19,14 +40,15 @@ export default class TodoListItems extends Component{
                   <div className="list-text"> 
                       <span 
                           style={ style }
-                          className="todo-list-item">
+                          className={ classNames }
+                          onClick={ this.onLabelClick }>
                           { label }
                       </span>
                   </div>
                   <div className="list-icons">
                       <span>
-                        <button type="button" className="btn"><i class="fas fa-trash-alt"></i></button>
-                        <button type="button" className="btn"><i class="fas fa-exclamation-triangle"></i></button>
+                        <button type="button" className="btn"><i className="fas fa-trash-alt"></i></button>
+                        <button type="button" className="btn"><i className="fas fa-exclamation-triangle"></i></button>
                       </span>
                   </div>
                 </div>
